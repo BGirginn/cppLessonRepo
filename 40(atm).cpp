ef852529e807db72843f4c96c04523f334a9834d
Bora Girgin <girginbora30@gmail.com>
2024-10-06 03:12:41 +0300

    new files

---
+++ 40(atm).cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+//* içindeki bakiyeyi görüntüleyip para yatırıp çekebildiğimiz bir banka uygulaması.
+
+// TODO Fatura ve benzeri işlemler eklenecek
+
+int main() {
+
+    return 0;
+}
+
+void menu() {
+    cout << "1. Bakiye Sorgula." << endl;
+    cout << "2. Para Yatır." << endl;
+}
