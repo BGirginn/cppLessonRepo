2a7418eb70ec0bd95dc8edc5a724371f84e5b65f
Bora Girgin <girginbora30@gmail.com>
2024-10-07 11:42:08 +0300

    using vscode and clion same time

---
+++ build/CMakeFiles/cpp.dir/cmake_clean.cmake
@@ -0,0 +1,39 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp.dir/05.cpp.obj"
+  "CMakeFiles/cpp.dir/05.cpp.obj.d"
+  "CMakeFiles/cpp.dir/11.cpp.obj"
+  "CMakeFiles/cpp.dir/11.cpp.obj.d"
+  "CMakeFiles/cpp.dir/14.cpp.obj"
+  "CMakeFiles/cpp.dir/14.cpp.obj.d"
+  "CMakeFiles/cpp.dir/15.cpp.obj"
+  "CMakeFiles/cpp.dir/15.cpp.obj.d"
+  "CMakeFiles/cpp.dir/20.cpp.obj"
+  "CMakeFiles/cpp.dir/20.cpp.obj.d"
+  "CMakeFiles/cpp.dir/22.cpp.obj"
+  "CMakeFiles/cpp.dir/22.cpp.obj.d"
+  "CMakeFiles/cpp.dir/32.cpp.obj"
+  "CMakeFiles/cpp.dir/32.cpp.obj.d"
+  "CMakeFiles/cpp.dir/33.cpp.obj"
+  "CMakeFiles/cpp.dir/33.cpp.obj.d"
+  "CMakeFiles/cpp.dir/35.cpp.obj"
+  "CMakeFiles/cpp.dir/35.cpp.obj.d"
+  "CMakeFiles/cpp.dir/36.cpp.obj"
+  "CMakeFiles/cpp.dir/36.cpp.obj.d"
+  "CMakeFiles/cpp.dir/37.cpp.obj"
+  "CMakeFiles/cpp.dir/37.cpp.obj.d"
+  "CMakeFiles/cpp.dir/38.cpp.obj"
+  "CMakeFiles/cpp.dir/38.cpp.obj.d"
+  "CMakeFiles/cpp.dir/39.cpp.obj"
+  "CMakeFiles/cpp.dir/39.cpp.obj.d"
+  "CMakeFiles/cpp.dir/40(atm).cpp.obj"
+  "CMakeFiles/cpp.dir/40(atm).cpp.obj.d"
+  "cpp.exe"
+  "cpp.exe.manifest"
+  "cpp.pdb"
+  "libcpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
