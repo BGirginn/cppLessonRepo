0270d08d10af04647ab51a4577cb4983489d0f2d
BGirginn <girginbora30@gmail.com>
2024-11-21 23:12:51 +0300

    21.11

---
+++ 52.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    int sayi[10];             // dizinin eleman sayısını yazdık içine
+    int sayi[] = {1, 2, 3, 4} // bir dizi tanımlarken ya eleman sayısını ya da elemanların ne olduğunu tanımlamak gerek
+
+    int diziEleman;
+    cin >> diziEleman      // bunun yerine int diziEleman = 10 diyip listeye onu tanımlayabilirsin
+    int sayi[diziEleman];  // dizi boyutlarını program çalışma esnasında değil önden tanımlı olarak yazmak
+
+
+        return 0;
+}
