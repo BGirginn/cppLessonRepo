571039a02960ce99d955c9376dd3a416c6142360
BGirginn <girginbora30@gmail.com>
2024-11-22 00:02:37 +0300

    22.11

--- 61.cpp
+++ 61.cpp
@@ -1,2 +1,25 @@
 //! POINTER
 
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+
+    // int sayi;
+    // int *sayiPointer = &sayi; // pointer değişken adi icin * bağlanacağı adres degeri icin & kullanılır
+
+    int sayi = 10;
+    int *sayiPointer;
+    sayiPointer = &sayi;  // ikisi de pointer ama burada tanımlarken başına * koymuyorsun
+
+    cout << *sayiPointer << endl;
+    cout << sayi << endl;
+
+    cout << sayiPointer << endl;
+    cout << &sayi << endl;
+
+    return 0;
+}
